a5d50a0a67026ce36f66c7dde1e86fb9283ff76e
Cool_Dude_Priyansh <priyanshsheth1997@gmail.com>
2016-12-23 21:09:28 +0530

    like table update

--- models/book_model.js
+++ models/book_model.js
@@ -13,7 +13,6 @@ var Book={
     {
       return db.query("Insert into book_tbl(fk_email_id,fk_event_id,ticket_cnt,ticket_amnt) values(?,?,?,?)",[Book.fk_email_id,Book.fk_event_id,Book.ticket_cnt,Book.ticket_amnt],callback);
     },
-
     deleteBook:function(id,callback)
     {
       return db.query("delete from book_tbl where pk_book_id=?",[id],callback);
